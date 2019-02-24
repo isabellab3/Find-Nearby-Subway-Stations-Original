1ea3ef10d43f130fd0dc82c77dd52a736dec1425
Isabella <isabellaberekdar@gmail.com>
2019-02-24 18:42:12 -0500

    modified views

--- subway/views.py
+++ subway/views.py
@@ -2,21 +2,21 @@ from django.shortcuts import render
 import requests
 
 def Home(request):
-    url = 'https://data.cityofnewyork.us/api/views/kk4q-3rt2/rows.json'
-    request = requests.get(url).json()
+    # url = 'https://data.cityofnewyork.us/api/views/kk4q-3rt2/rows.json'
+    # request = requests.get(url).json()
 
-    subway_data = []
+    # subway_data = []
 
-    for x in range(0, len(request['data'])):
-        station_info = {
-            'name' :      request['data'][x][10],
-            'latitude' :  request['data'][x][11][26:43],
-            'longitude' : request['data'][x][11][7:25],
-            'lines' :     request['data'][x][12],
-            'notes' :     request['data'][x][13],
-        }
-        subway_data.append(station_info)
+    # for x in range(0, len(request['data'])):
+    #     station_info = {
+    #         'name' :      request['data'][x][10],
+    #         'latitude' :  request['data'][x][11][26:43],
+    #         'longitude' : request['data'][x][11][7:25],
+    #         'lines' :     request['data'][x][12],
+    #         'notes' :     request['data'][x][13],
+    #     }
+    #     subway_data.append(station_info)
 
-    context = {'subway_data' : subway_data}
+    # context = {'subway_data' : subway_data}
 
     return render(request, 'home.html')
