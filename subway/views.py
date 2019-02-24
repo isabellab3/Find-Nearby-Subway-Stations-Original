7bbb08e2e0259c6b5a10d748dcd5ca7e80690909
Isabella <isabellaberekdar@gmail.com>
2019-02-24 18:48:47 -0500

    modified views

--- subway/views.py
+++ subway/views.py
@@ -3,20 +3,20 @@ import requests
 
 def Home(request):
     url = 'https://data.cityofnewyork.us/api/views/kk4q-3rt2/rows.json'
-    request = requests.get(url)
+    r = requests.get(url).json()
 
     subway_data = []
 
-    # for x in range(0, len(request['data'])):
-    #     station_info = {
-    #         'name' :      request['data'][x][10],
-    #         'latitude' :  request['data'][x][11][26:43],
-    #         'longitude' : request['data'][x][11][7:25],
-    #         'lines' :     request['data'][x][12],
-    #         'notes' :     request['data'][x][13],
-    #     }
-    #     subway_data.append(station_info)
+    for x in range(0, len(request['data'])):
+        station_info = {
+            'name' :      r['data'][x][10],
+            'latitude' :  r['data'][x][11][26:43],
+            'longitude' : r['data'][x][11][7:25],
+            'lines' :     r['data'][x][12],
+            'notes' :     r['data'][x][13],
+        }
+        subway_data.append(station_info)
 
-    # context = {'subway_data' : subway_data}
+    context = {'subway_data' : subway_data}
 
-    return render(request, 'home.html')
+    return render(request, 'home.html', context)
